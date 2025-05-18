cd7117f10fe25823bba88bb79073f79438ad46d7
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-05-18 22:29:29 +0200

    added required imgs and make the HTML

--- www/app/view/equipe.view.php
+++ www/app/view/equipe.view.php
@@ -2,32 +2,54 @@
     <ul>
         <li>
             <h1></h1>
-            <img src="../../public/images/orge.jpg" alt="">
+            <img src="../../public/images/equipe/mmi-maissa.png" alt="">
             <p></p>
         </li>
         <li>
             <h1></h1>
-            <img src="" alt="">
+            <img src="../../public/images/equipe/mmi-clemence.png" alt="">
             <p></p>
         </li>
         <li>
             <h1></h1>
-            <img src="" alt="">
+            <img src="../../public/images/equipe/mmi-clemence.png" alt="">
             <p></p>
         </li>
         <li>
             <h1></h1>
-            <img src="" alt="">
+            <img src="../../public/images/equipe/mmi-erwann.png" alt="">
             <p></p>
         </li>
         <li>
             <h1></h1>
-            <img src="" alt="">
+            <img src="../../public/images/equipe/mmi-rayan.png" alt="">
             <p></p>
         </li>
         <li>
             <h1></h1>
-            <img src="" alt="">
+            <img src="../../public/images/equipe/mmi-killian.png" alt="">
+            <p></p>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <h1></h1>
+            <img src="../../public/images/equipe/gb-mathilde.jpg" alt="">
+            <p></p>
+        </li>
+        <li>
+            <h1></h1>
+            <img src="../../public/images/equipe/gb-tom.jpg" alt="">
+            <p></p>
+        </li>
+        <li>
+            <h1></h1>
+            <img src="../../public/images/equipe/gb-matteo.jpg" alt="">
+            <p></p>
+        </li>
+        <li>
+            <h1></h1>
+            <img src="../../public/images/equipe/gb-mabel.png" alt="">
             <p></p>
         </li>
     </ul>
