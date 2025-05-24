8fda1dd276006d09a12f8e48cc165f4928305de1
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-05-24 11:33:01 +0200

    added more info for the DB connection

--- www/config.php
+++ www/config.php
@@ -1,5 +1,21 @@
 <?php
 
+/* 
+
+Database configuration
+
+define('DB_NAME', 'cdce5547_2025_symphony');
+
+define('DB_USER', 'cdce5547_admin_sql_symphony');
+
+define('DB_PASSWORD', 'MMI4ever@BDD');
+
+define('DB_HOST', 'localhost');
+
+define('DB_CHARSET', 'utf8');
+
+*/
+
 /** The name of the database for our website */
 define('DB_NAME', 'sae_db');
 
