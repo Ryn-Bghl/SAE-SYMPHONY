ee1e9b871f0e49b5f1842bdd1f8212266048daee
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-06-09 17:11:04 +0200

    fix: correct SQL dump metadata and table structure for consistency

--- cdce5547_2025_symphony.sql
+++ cdce5547_2025_symphony.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : ven. 06 juin 2025 à 15:52
--- Version du serveur : 8.4.3
--- Version de PHP : 8.3.16
+-- Hôte : localhost
+-- Généré le : lun. 09 juin 2025 à 17:10
+-- Version du serveur : 10.6.22-MariaDB
+-- Version de PHP : 8.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `sae_db`
+-- Base de données : `cdce5547_2025_symphony`
 --
 
 -- --------------------------------------------------------
@@ -28,13 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `biere` (
-  `id` int NOT NULL,
-  `nom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `gout` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `gout` varchar(50) DEFAULT NULL,
   `prix` decimal(3,2) DEFAULT NULL,
-  `genre` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `description` varchar(2000) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `police` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL
+  `genre` varchar(50) DEFAULT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `police` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -46,9 +46,9 @@ INSERT INTO `biere` (`id`, `nom`, `gout`, `prix`, `genre`, `description`, `polic
 (2, 'Tinnitus', 'Citron', 4.50, 'Techno', 'Citron électrique, acidité brute, fraîcheur rave.\nTinnitus, c’est une soirée techno dans chaque gorgée.\nUne bière qui résonne, même quand la musique s’arrête.', 'ROG'),
 (3, 'Distorsion', 'Piment', 4.50, 'Rock', 'Cette bière est dédiée aux vrais fans de rock, ceux qui vivent fort, qui hurlent dans la foule et vibrent au son des guitares saturées.', 'SIMBIOT'),
 (4, 'Mango And Explosion', 'Mangue', 4.50, 'Pop', 'Mango and Explosion, c’est une bière à la mangue qui explose en bouche comme un solo de trompette dans un concert de jazz. Vive, fruitée et pleine d’énergie, elle est faite pour les moments de fête et de liberté.', 'Okay Jelly'),
-(5, 'Blues N Berry', 'Myrtille', 4.50, 'Rn\'b', 'Avec Blue’s & Berry, Symphony signe une bière où le groove rencontre la gourmandise.\nImagine un solo de piano dans une cave enfumée, une myrtille juteuse entre les doigts.', 'Honey Salt'),
+(5, 'Blue\'s & Berry', 'Myrtille', 4.50, 'Rn\'b', 'Avec Blue’s & Berry, Symphony signe une bière où le groove rencontre la gourmandise.\nImagine un solo de piano dans une cave enfumée, une myrtille juteuse entre les doigts.', 'Honey Salt'),
 (6, 'Harmony', 'Pomme', 4.50, 'Classique', 'Harmony, bière à la pomme douce et pétillante, accompagne parfaitement un moment de calme et de musique classique. Chaque gorgée en évoque la justesse et l’élégance.', 'Montserrat'),
-(7, 'Beer\'oco', 'Cacao', 4.50, 'Hip Hop', 'Beer’oco est une bière au style hip-hop au goût cacao, mêlant douceur chocolatée et caractère. Idéale pour les amateurs de saveurs originales et d’univers urbain, elle se déguste en rythme.', 'Bernard Mt');
+(7, 'Beer\'oco', 'Cacao', 4.50, 'HipHop', 'Beer’oco est une bière au style hip-hop au goût cacao, mêlant douceur chocolatée et caractère. Idéale pour les amateurs de saveurs originales et d’univers urbain, elle se déguste en rythme.', 'Bernard Mt');
 
 -- --------------------------------------------------------
 
@@ -57,9 +57,9 @@ INSERT INTO `biere` (`id`, `nom`, `gout`, `prix`, `genre`, `description`, `polic
 --
 
 CREATE TABLE `team` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `citation` text
+  `citation` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `team` (
 --
 
 INSERT INTO `team` (`id`, `name`, `citation`) VALUES
-(1, 'Rayan', 'std::cout << \"Hello, World!\" << std::endl; - John Doe'),
+(1, 'Rayan', 'std::cout << \'Hello, World!\' << std::endl; - John Doe'),
 (2, 'Clémence', 'Une journée sans rire est une journée gachée - Clémence'),
 (3, 'Candice', 'Pas de tenders pas de travail - Candice'),
 (4, 'Killian', 'Les gens ne cesseront jamais d’avoir des rêves ! - Killian'),
@@ -81,16 +81,24 @@ INSERT INTO `team` (`id`, `name`, `citation`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilsateur`
+-- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilsateur` (
-  `id` int NOT NULL,
-  `nom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `prenom` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `email` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `mdp` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mdp` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `mdp`) VALUES
+(1, 'doe', 'john', 'john.doe@gmail.com', '$2y$10$ewei8LehSVw.v7Gt52q4FOu/1KpBfwSCbEm.66zr0DOe3ShOUhIqi'),
+(2, 'rayan', 'boughalia', 'boughalia.rayan@gmail.com', '$2y$10$fsPhrIh0uOjwN0wnxJet5eT2FMbCmfE165eftfK/lYO1V4tn47avW');
 
 --
 -- Index pour les tables déchargées
@@ -109,9 +117,9 @@ ALTER TABLE `team`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `utilsateur`
+-- Index pour la table `utilisateur`
 --
-ALTER TABLE `utilsateur`
+ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,19 +130,19 @@ ALTER TABLE `utilsateur`
 -- AUTO_INCREMENT pour la table `biere`
 --
 ALTER TABLE `biere`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `team`
 --
 ALTER TABLE `team`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT pour la table `utilsateur`
+-- AUTO_INCREMENT pour la table `utilisateur`
 --
-ALTER TABLE `utilsateur`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
